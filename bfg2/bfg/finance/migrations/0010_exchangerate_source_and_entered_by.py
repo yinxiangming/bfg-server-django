e7a84b35faeb80aaac42b26379f67c0a18347f74
Mark Yin <yinxiangming@gmail.com>
2026-09-23 22:32:12 +1200

    fix: inspect the database column for foreign keys

--- bfg2/bfg/finance/migrations/0010_exchangerate_source_and_entered_by.py
+++ bfg2/bfg/finance/migrations/0010_exchangerate_source_and_entered_by.py
@@ -13,7 +13,8 @@ class AddFieldIfMissing(migrations.AddField):
             columns = schema_editor.connection.introspection.get_table_description(
                 cursor, model._meta.db_table
             )
-        return self.name in {column.name for column in columns}
+        field = model._meta.get_field(self.name)
+        return field.column in {column.name for column in columns}
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         model = to_state.apps.get_model(app_label, self.model_name)
